687544b9f5beb5d7247cbd9c9766030bd47d68ab
Vilgad <rohitgupta16685@gmail.com>
2024-09-14 10:08:28 +0530

    Project setup complete

---
+++ src/main/java/com/snippet/gig/GigApplication.java
@@ -0,0 +1,13 @@
+package com.snippet.gig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snippet/gig/GigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snippet.gig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
