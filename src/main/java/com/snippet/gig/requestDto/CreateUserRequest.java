5f322a610ea13d8d1ad7334a47763def143c0429
vilgad <rohitgupta16685@gmail.com>
2024-10-13 14:56:36 +0530

    added validation annotations in CreateUserRequest.java

--- src/main/java/com/snippet/gig/requestDto/CreateUserRequest.java
+++ src/main/java/com/snippet/gig/requestDto/CreateUserRequest.java
@@ -1,13 +1,37 @@
 package com.snippet.gig.requestDto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Objects;
 
 public class CreateUserRequest {
+    @NotNull(message = "name cannot be null")
+    @NotEmpty(message = "name cannot be empty")
     private String name;
+
+    @DateTimeFormat
     private String dob;
+
+    @NotEmpty(message = "username cannot be empty")
+    @NotNull(message = "username cannot be null")
+    @Size(min = 6, message = "username must be of at-least 6 characters")
     private String username;
+
+    @Email(message = "provide a valid email address")
+    @NotNull(message = "email cannot be null")
     private String email;
+
+    @NotNull(message = "password cannot be null")
+    @NotEmpty(message = "password cannot be empty")
+    @Size(min = 8, message = "password must be of at-least 8 characters")
     private String password;
+
+    @NotNull(message = "role cannot be null")
+    @NotEmpty(message = "role cannot be empty")
     private String role;
 
     public CreateUserRequest() {
